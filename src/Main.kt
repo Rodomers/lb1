fd9252a3b4e6031194937d8b6d3889b22f5c4387
Rodomers <g.rodomers@gmail.com>
2025-09-16 18:09:29 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,23 @@
+class Enterprise(
+    val name: String,
+    private val employees: Int,
+    private val prodRate: Int,
+    private val hourFee: Float,
+    private val incomeTax: Float
+) {
+    fun countPayout(): Int {
+        return (employees * prodRate * hourFee * incomeTax).toInt()
+    }
+}
+
+fun main(args: Array<String>) {
+    val enterprise = Enterprise(
+        name = "Global Innovations Ltd.",
+        employees = 150,
+        prodRate = 8,
+        hourFee = 25.50f,
+        incomeTax = 0.85f
+    )
+
+    println(enterprise.countPayout())
+}
